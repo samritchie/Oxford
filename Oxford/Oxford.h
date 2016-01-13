c964d481f6177f754a222e0500a259aed65b7092
Sam Ritchie <sam@codesplice.com.au>
2016-01-13 21:04:12 +0800

    Initial Commit

---
+++ Oxford/Oxford.h
@@ -0,0 +1,19 @@
+//
+//  Oxford.h
+//  Oxford
+//
+//  Created by Sam Ritchie on 13/01/2016.
+//  Copyright © 2016 codesplice pty ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Oxford.
+FOUNDATION_EXPORT double OxfordVersionNumber;
+
+//! Project version string for Oxford.
+FOUNDATION_EXPORT const unsigned char OxfordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Oxford/PublicHeader.h>
+
+
